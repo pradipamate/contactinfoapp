892b535ccf08c35c7a87a0c1c4b51563c12c9659
5377 <pradipamate07@gmail.com>
2020-08-16 19:08:17 +0530

    Updates

---
+++ precache-manifest.877b590a14a9f9b10aa559e45687c4b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "95227dc95f80312c3ac5",
+    "url": "/contactinfoapp/static/css/main.8fcd3d69.chunk.css"
+  },
+  {
+    "revision": "95227dc95f80312c3ac5",
+    "url": "/contactinfoapp/static/js/main.7e3b02e4.chunk.js"
+  },
+  {
+    "revision": "f4575273947bbbf347de",
+    "url": "/contactinfoapp/static/js/runtime~main.0fde190f.js"
+  },
+  {
+    "revision": "f084227852da4319503e",
+    "url": "/contactinfoapp/static/css/2.5f8c356b.chunk.css"
+  },
+  {
+    "revision": "f084227852da4319503e",
+    "url": "/contactinfoapp/static/js/2.8be375ff.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contactinfoapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contactinfoapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contactinfoapp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contactinfoapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contactinfoapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f483904efd580a32715f78c1458fe20e",
+    "url": "/contactinfoapp/index.html"
+  }
+];
